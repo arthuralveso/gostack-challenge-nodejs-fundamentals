0acfc449a849b441bc1d6f6e305c87567df385ed
Arthur Alves <arthuralves91@hotmail.com>
2020-07-05 19:55:31 -0300

    insere e lista trasaçoes financeiras e balanço

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+/* eslint-disable no-return-assign */
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,55 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  public compareType(type: string): string | false {
+    if (type === 'income' || type === 'outcome') {
+      return type;
+    }
+    return false;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance: Balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+
+    this.transactions.reduce((accumulator, object) => {
+      if (object.type === 'income') {
+        // eslint-disable-next-line no-param-reassign
+        return (balance.income += object.value);
+      }
+      if (object.type === 'outcome') {
+        return (balance.outcome += object.value);
+      }
+      return accumulator;
+    }, 0);
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public invalidTransation(value: number, type: string): number | null {
+    const balance = this.getBalance();
+
+    if (value > balance.total && type === 'outcome') {
+      return null;
+    }
+    return value;
+  }
+
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,28 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: Request): Transaction {
+    const isCorrectType = this.transactionsRepository.compareType(type);
+    const isValidTransation = this.transactionsRepository.invalidTransation(
+      value,
+      type,
+    );
+
+    if (isCorrectType === false) {
+      throw Error('Incorrect type. Must be income or outcome');
+    }
+
+    if (isValidTransation === null) {
+      throw Error('Invalid transation');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+
+    return transaction;
   }
 }
 
